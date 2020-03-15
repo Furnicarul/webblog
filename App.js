7bdbbf117abf8707b9d2dce22227a1228d412b8b
Furnicarul <schifirnetiulian2003@yahoo.com>
2020-03-15 17:46:41 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./style.css"
+
+class App extends React.Component {
+  render(){
+    return(
+      <div className="div">
+        <h1>Hello</h1>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-crqcqq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-crqcqq)
