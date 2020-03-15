263262dc7f05b6820128b069c4f76f6d9410515b
Furnicarul <schifirnetiulian2003@yahoo.com>
2020-03-15 17:55:17 +0200

    Added the layout.

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import React from "react"
 import "./style.css"
 
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl"
+
 class App extends React.Component {
   render(){
     return(
-      <div className="div">
-        <h1>Hello</h1>
-      </div>
+      <div className="demo-big-content">
+        <Layout>
+            <Header title="Title" scroll>
+                <Navigation>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                </Navigation>
+            </Header>
+            <Drawer title="Title">
+                <Navigation>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                </Navigation>
+            </Drawer>
+            <Content>
+                <div className="page-content" />
+            </Content>
+        </Layout>
+</div>
     )
   }
 }
