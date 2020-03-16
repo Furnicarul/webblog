220d6fa610458e29b921df10c3123730e5b38f15
Furnicarul <schifirnetiulian2003@yahoo.com>
2020-03-16 19:58:16 +0200

    Modified the projects.js file

--- components/projects.js
+++ components/projects.js
@@ -1,17 +1,28 @@
 import React from "react"
 
-import { Tabs, Tab } from "react-mdl"
+import { Tabs, Tab, Grid, Cell } from "react-mdl"
 
 class Projects extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      activeTab: 0
-    }
-
- 
+    this.state = {activeTab: 0}
   }
 
+  toggleCategories() {
+    if (this.state.activeTab === 0 ) {
+      return(
+        <div><h1>React Template</h1></div>
+      )
+    } else if(this.state.activeTab === 1) {
+      return(
+        <div><h1>Angular Template</h1></div>
+      )
+    } else {
+      return(
+        <div><h1>PUG Template</h1></div>
+      )
+    }
+  }
 
   render() {
     return(
@@ -22,7 +33,13 @@ class Projects extends React.Component {
           <Tab>PUG</Tab>
         </Tabs>
         <section className="projects-grid">
-          {this.toggleCategories()}
+          <Grid className="projects-grid">
+            <Cell col={12}>
+              <div className="content">
+                {this.toggleCategories()}
+              </div>
+            </Cell>
+          </Grid>
         </section>
       </div>
     )
