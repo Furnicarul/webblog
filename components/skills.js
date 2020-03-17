f96b3662f27bfbc349bf01e12b36709409297d0a
Furnicarul <schifirnetiulian2003@yahoo.com>
2020-03-17 14:25:07 +0200

    4 files modified

--- components/skills.js
+++ components/skills.js
@@ -1,10 +1,13 @@
 import React from "react"
 
+import { Grid, Cell, ProgressBar } from "react-mdl"
+
 class Skills extends React.Component{
   render(){
     return(
-      <div className="div">
-        <h1>Skills page !</h1>
+      <div className="skills-body">
+        <div className="skills-grid">
+        </div>
       </div>
     )
   }
